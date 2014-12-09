3022ce4949aacf7edfd49e7dccf53c402f73afb5
Jorge Quilcate <quilcate.jorge@gmail.com>
2014-12-09 12:02:05 -0500

    version updated

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'quilcate.jorge@gmail.com'
 license          'Apache 2.0'
 description      'Installs/Configures Oracle Fusion Middleware'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 depends 'java'
 
